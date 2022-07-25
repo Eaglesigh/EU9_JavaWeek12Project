c1e640586361b6d3db7245244c1ddaab3c2ed08f
Eaglesigh <yasarmurat76@gmail.com>
2022-07-25 11:52:50 +0300

    For Loop Edit

--- src/constructers/Example.java
+++ src/constructers/Example.java
@@ -2,6 +2,10 @@ package constructers;
 
 public class Example {
     public static void main(String[] args) {
-
+     int n =0;
+        for (int i = 0; i < 10; i++) {
+            n++;
+            System.out.print(n);
+        }
     }
 }
